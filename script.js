411ad3a502e421182a72cc0878f91f0208af0325
Darshan Raut <56134448+darshanraut123@users.noreply.github.com>
2021-09-19 00:26:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+setTimeout(() => {
+        document.body.innerText=10;
+    setTimeout(()=>{
+        document.body.innerText=9;
+        setTimeout(()=>{
+            document.body.innerText=8;
+            setTimeout(()=>{
+                document.body.innerText=7;
+                setTimeout(()=>{
+                    document.body.innerText=6;
+                    setTimeout(()=>{
+                        document.body.innerText=5;
+                        setTimeout(()=>{
+                            document.body.innerText=4;
+                            setTimeout(()=>{
+                                document.body.innerText=3;
+                                setTimeout(()=>{
+                                    document.body.innerText=2;
+                                    setTimeout(()=>{
+                                        document.body.innerText=1;
+                                        setTimeout(()=>{
+                                            document.body.innerText="Happy New Year!";                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+}, 1000);
